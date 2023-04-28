7a9179782f8023c1683f68f12dc0472c94e9bdad
Nikola Hristov <nikola@nikolahristov.tech>
2023-04-28 17:33:58 +0300

    squash!

--- test/index.sql
+++ test/index.sql
@@ -1,12 +1,18 @@
-WITH table_1 AS
-				(SELECT title,
-						country,
-						salary + payroll_tax + benefits_cost AS _expr_0,
-						salary + payroll_tax AS _expr_1,
-						salary
-					FROM employees
-					WHERE start_date > DATE '2021-01-01' )
-SELECT title,
+WITH
+	table_1 AS (
+		SELECT
+			title,
+			country,
+			salary + payroll_tax + benefits_cost AS _expr_0,
+			salary + payroll_tax AS _expr_1,
+			salary
+		FROM
+			employees
+		WHERE
+			start_date > DATE '2021-01-01'
+	)
+SELECT
+	title,
 	country,
 	AVG(salary),
 	SUM(salary),
@@ -15,11 +21,17 @@ SELECT title,
 	AVG(_expr_0),
 	SUM(_expr_0) AS sum_gross_cost,
 	COUNT(*) AS ct
-FROM table_1
-WHERE _expr_0 > 0
-GROUP BY title,
+FROM
+	table_1
+WHERE
+	_expr_0 > 0
+GROUP BY
+	title,
 	country
-HAVING COUNT(*) > 2000
-ORDER BY sum_gross_cost,
+HAVING
+	COUNT(*) > 2000
+ORDER BY
+	sum_gross_cost,
 	country DESC
-LIMIT 20
+LIMIT
+	20
